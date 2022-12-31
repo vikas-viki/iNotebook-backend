9ae4c87f349e4ee4d6e138a9abc9dc2f060e3826
vikas <manojkotary@gmail.com>
2022-12-31 13:15:41 +0530

    online integration

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb+srv://VIKAS:VIKAS@firstcluster/inotebook.1fyyzpk.mongodb.net/?retryWrites=true&w=majority"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
